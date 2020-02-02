7aba0d91d13484a060077cfe4e9a42686d538574
delianides <github@delianides.com>
2020-02-01 20:37:58 -0700

    updated storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.[tj]s'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-notes',
-    '@storybook/addon-links',
-    '@storybook/addon-docs'
+    '@storybook/addon-links/register',
+    '@storybook/addon-docs/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-notes/register'
   ],
   webpackFinal: async config => {
     config.module.rules.push({
